fed4b5bc6d70f8ddd381a98a09148a75c8b6178d
Craluminum2413 <beware.14b@gmail.com>
2023-09-02 18:01:22 +0300

    check for projectiles with "projectile" in name

--- src/Systems/Core.cs
+++ src/Systems/Core.cs
@@ -9,17 +9,28 @@ namespace GlowingProjectiles;
 
 public class Core : ModSystem
 {
+    private ICoreClientAPI capi;
+
+    public override bool ShouldLoad(EnumAppSide forSide) => forSide == EnumAppSide.Client;
+
     public override void StartClientSide(ICoreClientAPI api)
     {
         base.StartClientSide(api);
+        capi = api;
 
         api.Event.OnEntitySpawn += SetGlowLevel;
         api.World.Logger.Event("started '{0}' mod", Mod.Info.Name);
     }
 
+    public override void Dispose()
+    {
+        base.Dispose();
+        capi.Event.OnEntitySpawn -= SetGlowLevel;
+    }
+
     private void SetGlowLevel(Entity entity)
     {
-        if (entity is EntityProjectile)
+        if (entity is EntityProjectile || entity.Class.Contains("projectile"))
         {
             entity.Properties.Client.GlowLevel = 255;
         }
